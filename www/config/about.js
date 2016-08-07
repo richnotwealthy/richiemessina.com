b63bd9c417269506ed6611033917a675e5f9b092
Richie <messina.rd@gmail.com>
2016-08-07 02:10:15 -0400

    more additions to about page

--- www/config/about.js
+++ www/config/about.js
@@ -2,8 +2,7 @@ module.exports =
 {
    title: 'About Me',
    me: '../images/me.png',
-   body: "I am currently an undergraduate student pursuing my BS in Computer Science & \
-   Engineering at New York University. Deeply passionate about modern software, I simply love\
+   body: "Deeply passionate about modern software, I simply love\
    developing programs and applications with expansive utility and attractive architecture.",
    infos: [
        {
@@ -28,26 +27,171 @@ module.exports =
            developing programs and applications with expansive utility and attractive architecture."
        }
    ],
+   jobs: [
+       {
+           title: "Techonology Development Intern",
+           text: "I am currently an undergraduate student pursuing my BS in Computer Science & \
+           Engineering at New York University. Deeply passionate about modern software, I simply love\
+           developing programs and applications with expansive utility and attractive architecture"
+       },
+       {
+           title: "Media Support Technician",
+           text: "I am currently an undergraduate student pursuing my BS in Computer Science & \
+           Engineering at New York University. Deeply passionate about modern software, I simply love\
+           developing programs and applications with expansive utility and attractive architecture"
+       },
+       {
+           title: "Inventory Management Associate",
+           text: "I am currently an undergraduate student pursuing my BS in Computer Science & \
+           Engineering at New York University. Deeply passionate about modern software, I simply love\
+           developing programs and applications with expansive utility and attractive architecture"
+       }
+   ],
    quals: [
        {
-           header: "Languages",
+           header: "Technologies",
            items: [
                {
-                   img: "class",
-                   name: "JavaScript",
+                   img: "../images/terminal.png",
+                   name: "Bash Terminal",
+                   grade: [,,,,,]
+               },
+               {
+                   img: "../images/git.png",
+                   name: "Git",
+                   grade: [,,,,,]
+               },
+               {
+                   img: "../images/nodejs.png",
+                   name: "Node.js",
+                   grade: [,,,,,]
+               },
+               {
+                   img: "../images/jquery.png",
+                   name: "jQuery",
+                   grade: [,,,,,]
+               },
+               {
+                   img: "../images/elasticsearch.png",
+                   name: "Elasticsearch",
+                   grade: [,,,,]
+               },
+               {
+                   img: "../images/gulp.png",
+                   name: "Gulp",
+                   grade: [,,,,]
+               },
+               {
+                   img: "../images/express.png",
+                   name: "Express",
+                   grade: [,,,,]
+               },
+               {
+                   img: "../images/dust.png",
+                   name: "Dust.js",
+                   grade: [,,,,]
+               },
+               {
+                   img: "../images/bootstrap.png",
+                   name: "Bootstrap",
+                   grade: [,,,,]
+               },
+               {
+                   img: "../images/materialize.svg",
+                   name: "Materialize",
+                   grade: [,,,,]
+               },
+               {
+                   img: "../images/bower.png",
+                   name: "Bower",
+                   grade: [,,,,]
+               },
+               {
+                   img: "../images/docker.png",
+                   name: "Docker",
                    grade: [,,,,]
                }
            ]
        },
        {
-           header: "Technologies",
+           header: "Languages",
            items: [
                {
-                   img: "class",
-                   name: "Dust.js",
+                   img: "../images/javascript.jpeg",
+                   name: "JavaScript",
+                   grade: [,,,,,]
+               },
+               {
+                   img: "../images/python.png",
+                   name: "Python",
+                   grade: [,,,,,]
+               },
+               {
+                   img: "../images/cplusplus.png",
+                   name: "C++",
+                   grade: [,,,,,]
+               },
+               {
+                   img: "../images/html.png",
+                   name: "HTML",
+                   grade: [,,,,]
+               },
+               {
+                   img: "../images/css.png",
+                   name: "CSS",
+                   grade: [,,,,]
+               },
+               {
+                   img: "../images/sass.png",
+                   name: "Sass",
+                   grade: [,,,,]
+               },
+               {
+                   img: "../images/java.png",
+                   name: "Java",
                    grade: [,,,]
                }
            ]
+       },
+       {
+           header: "Miscellaneous",
+           items: [
+               {
+                   img: "../images/msoffice.png",
+                   name: "Microsoft Office",
+                   grade: [,,,,,]
+               },
+               {
+                   img: "../images/gdrive.png",
+                   name: "Google Drive",
+                   grade: [,,,,,]
+               },
+               {
+                   img: "../images/social.png",
+                   name: "Social Media",
+                   grade: [,,,,,]
+               }
+           ]
+       }
+   ],
+   schools: [
+       {
+           title: "New York University",
+           dates: "2015 - 2019 (Present)",
+           text: "I'm currently pursuing a BS in Computer Science at the NYU Tandon School of Engineering.\
+           So far, I've achieved Dean's List placement for the 2015 - 2016 academic year with a 3.99 GPA.\
+           I've served as member of Hall Council, Design for America NYU, and the Entrepreneurship & Innovation Association.",
+           img: "../images/nyu.jpg"
+       },
+       {
+           title: "Saint Peter's Preparatory High School",
+           dates: "2011 - 2015",
+           text: "In high school, I served as President of the Prep Against Ignorance Club, Secretary of the\
+           National Spanish Honor Society, member of the National Honor Society, and a member of the Computer Club.\
+           I graduated with Distinguished Honors (GPA of 4.4 on a 4.5 scale, unweighted), was the ecipient of\
+           4 Academic Achievement awards granted annually, and acted as both a Sheehan Scholar (full merit\
+           scholarship program) and an Ignatian Scholar.",
+           img: "../images/spprep.png"
        }
    ]
 }
